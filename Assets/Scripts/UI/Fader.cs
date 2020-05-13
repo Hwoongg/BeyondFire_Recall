94f8a7cde81d2dd9c15378b534f1d1ebdba052c3
hwoongg <gusdnd8536@gmail.com>
2020-05-14 06:50:53 +0900

    페이더 초기화 타이밍 Start -> Awake로 앞당김

--- Assets/Scripts/UI/Fader.cs
+++ Assets/Scripts/UI/Fader.cs
@@ -9,13 +9,13 @@ using UnityEngine;
 public class Fader : MonoBehaviour
 {
     FadeEffect fadeEffect;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         // 자식에 붙은 페이드 이펙트 컴포넌트 획득
         fadeEffect = transform.GetChild(0).GetComponent<FadeEffect>();
     }
-
+    
     public void FadeIn()
     {
         fadeEffect.FadeIn();
