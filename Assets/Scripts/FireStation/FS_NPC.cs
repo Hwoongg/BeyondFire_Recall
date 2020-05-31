f12fc481f869adc4dc868a6b7af63615ac76fb0b
hwoongg <gusdnd8536@gmail.com>
2020-05-31 20:59:54 +0900

    소방서 씬 플레이 가능하도록 수정. NPC들에 대사 넣을 수 있도록 컴포넌트 추가.

---
+++ Assets/Scripts/FireStation/FS_NPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FS_NPC : InteractionSystem
+{
+    [SerializeField] Dialogue dialogue;
+    bool isReady;
+
+    private void Start()
+    {
+        isReady = true;
+    }
+    public override void doAction()
+    {
+        if (isReady)
+        {
+            DialogueManager.Instance().StartDialogue(dialogue);
+            isReady = false;
+            FindObjectOfType<FireStationManager>().CountDown();
+        }
+    }
+
+    public override void upAction()
+    {
+        doAction();
+    }
+
+    public override void downAction()
+    {
+        doAction();
+    }
+}
---
+++ Assets/Scripts/FireStation/FireStationManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireStationManager : MonoBehaviour
+{
+    int talkCount;
+
+    private void Start()
+    {
+        talkCount = FindObjectsOfType<FS_NPC>().Length;
+    }
+
+    public void CountDown()
+    {
+        talkCount--;
+
+        if(talkCount <= 0)
+        {
+            // 씬 종료 연출
+        }
+    }
+}
