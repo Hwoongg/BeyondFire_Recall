4da0fa152be39d3f911cdbef0e43b7654dccb166
hwoongg <gusdnd8536@gmail.com>
2020-06-14 13:42:34 +0900

    게임 오버 창 연출 추가

--- Assets/Scripts/SceneManager/SCM_GameOver.cs
+++ Assets/Scripts/SceneManager/SCM_GameOver.cs
@@ -6,18 +6,69 @@ using UnityEngine.SceneManagement;
 public class SCM_GameOver : MonoBehaviour
 {
     GameManager gm;
+    [SerializeField] GameObject coverObj;
+    Vector4 transColor;
+    [SerializeField] float fadeSpeed = 1;
+
+    delegate void FadeCallBack();
+
+    bool isFading;
+
+    private void Awake()
+    {
+        isFading = false;
+    }
 
     private void Start()
     {
         gm = GameManager.Instance();
+
+        StartCoroutine(UIAnimator.Color(coverObj, Color.black, Vector4.zero, fadeSpeed));
     }
+
     public void RestartEvent()
     {
-        SceneManager.LoadScene(gm.GetLastScene());
+        if (isFading)
+            return;
+
+        StopAllCoroutines();
+
+        StartCoroutine(
+            FadeOutRoutine(Restart));
+    }
+
+    void Restart()
+    {
+        string sceneName = gm.GetLastScene();
+
+        if (sceneName == null)
+        {
+            Debug.Log("Last Scene Is Notihing");
+            return;
+        }
     }
 
     public void MainMenuEvent()
     {
+        if (isFading)
+            return;
+
+        StopAllCoroutines();
+
+        StartCoroutine(
+            FadeOutRoutine(GotoMainMenu));
+    }
+
+    void GotoMainMenu()
+    {
         SceneManager.LoadScene("MainMenu_v2");
+    }
+
+    IEnumerator FadeOutRoutine(FadeCallBack fadeCallBack)
+    {
+        isFading = true;
+        yield return UIAnimator.Color(coverObj, Vector4.zero, Color.black, fadeSpeed * 2);
+
+        fadeCallBack();
     }
 }
